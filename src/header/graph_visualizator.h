608349bbaaa3c211366e31efaa029e129b0c7009
SaharaSurfer <st119029@student.spbu.ru>
2024-02-22 12:41:12 +0300

    The functions written earlier are deleted because they are no longer needed due to the change of the graph laying algorithm.

--- src/header/graph_visualizator.h
+++ src/header/graph_visualizator.h
@@ -11,26 +11,6 @@ public:
 
   void ReadGraph(const std::string& filename);
 
-  std::vector<std::vector<size_t>> SearchForConnectivityComponents();
-  void FindConnectivityComponent(
-      const size_t& vertex_ind,
-      std::vector<bool> *visited,
-      std::vector<std::vector<size_t>> *connectivity_components);
-
-  std::vector<std::pair<size_t, size_t>> SearchForBridges();
-  void FindBridge(const size_t& vertex_ind,
-                  const size_t& parent_ind,
-                  std::vector<bool> *visited,
-                  std::vector<size_t> *time_in,
-                  std::vector<size_t> *min_time_in_reachable,
-                  std::vector<std::pair<size_t, size_t>> *bridges);
-  
-  std::vector<size_t> GetCycle();
-  bool DetectCycle(const size_t& vertex_ind,
-                   size_t *cycle_start, size_t *cycle_end,
-                   std::vector<char> *vertex_color,
-                   std::vector<size_t> *parents);
-
 private:
   size_t vertex_num_ = 0;
   size_t edge_num_ = 0;
