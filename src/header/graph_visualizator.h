65c30590c67daf24a6eab8fda0803a2736e841be
SaharaSurfer <st119029@student.spbu.ru>
2024-03-02 12:28:46 +0300

    Removed unnecessary functions due to the change of graph visualisation algorithm.

--- src/header/graph_visualizator.h
+++ src/header/graph_visualizator.h
@@ -2,10 +2,8 @@
 #define GRAPHVISUALIZATION_HEADER_GRAPH_VISUALIZATOR_H_
 
 #include <cstdint>
-#include <deque>
 #include <memory>
 #include <string>
-#include <utility>
 #include <vector>
 
 // Vertex struct represents a vertex in a graph.
@@ -18,9 +16,6 @@ struct Vertex {
   double x = 0.0;
   double y = 0.0;
   std::vector<std::shared_ptr<Vertex>> neighbours;
-  
-  // The highest number of the filtration set that contains the given vertex.
-  int depth = 0;
 };
 
 // GraphVisualizator class is responsible for visualizing graphs.
@@ -45,56 +40,12 @@ private:
   // - a vector containing the distances from the root to all vertices
   std::vector<size_t> BFS(std::shared_ptr<Vertex> root);
 
-  // CreateVertexFiltration function generates a vertex filtration 
-  // (V = V_0 > V_1 > ... > V_k > 0) for the graph using a random process.
-  // Each V_i is a maximal subset of V_{i - 1} for which the graph distance
-  // between any pair of its elements is at least 2^{i - 1} + 1.
-  // The function swaps the elements of the graph so that the first |V_k| 
-  // elements belong to V_k, the second |V_{k-1}| elements 
-  // belong to V_{k-1}, and so on.
-  // Returns:
-  // - a vector containing the sizes of each filtration stage
-  std::vector<size_t> CreateVertexFiltration();
-
-  // Finds sets of nearest neighbors of a graph vertex for each filter set.
-  // Parameters:
-  //   - borders: A vector containing the boundaries of each filter set.
-  //   - root: The root vertex from which to start the search.
-  // Returns:
-  // A vector of vectors containing the nearest neighbor sets for each filter set.
-  std::vector<std::vector<std::shared_ptr<Vertex>>>
-  FindNearestNeighbourSets(const std::vector<size_t>& borders,
-                           std::shared_ptr<Vertex> root);
-
-  // PlaceCoreVertices function calculates the positions of 
-  // core vertices in the graph.
-  void PlaceCoreVertices();
-
-  // Finds the three nearest vertices to a given root vertex in the graph.
-  // Parameters:
-  //   - borders: A vector containing the boundaries of each filter set.
-  //   - root: The root vertex from which to start the search.
-  // Returns:
-  // A vector of pairs containing the distance to root and the index in graph_.
-  std::vector<std::pair<size_t, size_t>>
-  FindThreeNearestVertices(const std::vector<size_t>& borders,
-                           std::shared_ptr<Vertex> root);
-
   size_t vertex_num_ = 0;
   size_t edge_num_ = 0;
   std::vector<std::shared_ptr<Vertex>> graph_;
 
   // Desired length of an edge in the visualization
   const size_t kEdgeLen_ = 30;
-
-  // Number of elements to remain in V_k
-  const size_t kCoreVertexNumber_ = 3;
-
-  // Number of repetitions before resetting last set of filtration
-  const size_t kRepeatBeforeReset_ = 1000;
-  
-  // Threshold for resetting filtration
-  const size_t kResetThreshhold_ = 0;
 };
 
 #endif  // GRAPHVISUALIZATION_HEADER_GRAPH_VISUALIZATOR_H_
